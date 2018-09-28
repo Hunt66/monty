96b1347c897cbb9198faedf74eec704ab72ad6c4
alex farioletti <gsf.nyc@gmail.com>
2018-09-28 10:02:14 -0700

    not too short

--- mul.c
+++ mul.c
@@ -11,7 +11,7 @@ void mul(stack_t **stck, unsigned int line_number)
 {
 	stack_t *current = NULL;
 
-	if (*stck == NULL || (*stck)->next == NULL || (*stck)->next->next == NULL)
+	if (*stck == NULL || (*stck)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
 		free_all(stck, line_number);
